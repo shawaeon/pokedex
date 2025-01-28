6c3fd1d09c5eaaf1a14c2c997a50577591b1ed96
shawaeon <jens.sjogr@gmail.com>
2025-01-28 15:42:44 +0200

    Add user input

--- repl.go
+++ repl.go
@@ -1,6 +1,27 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)	
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		command := words[0]
+		fmt.Printf("Your command was: %v\n", command)
+	}
+}
 
 // Convert to lowercase and split based on whitespaces
 func cleanInput(text string) []string {
