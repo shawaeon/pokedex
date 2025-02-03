8260f08ab3495e589abfe8e98a09c401404c82ba
shawaeon <jens.sjogr@gmail.com>
2025-02-03 14:34:57 +0200

    Add tests for pokeball

---
+++ internal/pokeball/pokeball_test.go
@@ -0,0 +1,32 @@
+package pokeball
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAddGet(t *testing.T) {
+	cases := []struct {
+		key	string
+		val	Pokemon
+	} {
+		{
+			key:	"pikachu",
+			val:	Pokemon{Name: "pikachu"},
+		},
+		{
+			key:	"charmander",
+			val:	Pokemon{Name: "charmander"},
+		},
+	}
+	for i, c := range(cases) {
+		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T){
+			pokeball := NewPokeball()
+			pokeball.Add(c.key, c.val)
+			val := pokeball.Get(c.key)
+			if val.Name != c.val.Name {
+				t.Errorf("expected to find a value")
+			}
+		})
+	}
+}
